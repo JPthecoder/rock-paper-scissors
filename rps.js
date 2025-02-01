1b4501045824ae1ac89ec7c39400910e5d531556
Johnson Phan <175180662+JPthecoder@users.noreply.github.com>
2025-02-01 14:05:39 -0800

    Wrote and Tested playRound function

--- rps.js
+++ rps.js
@@ -13,32 +13,77 @@ function getComputerChoice(){
 }
 
 let question = prompt("Choose Rock, Paper, or Scissors", '');
-function getHumanChoice(){
+    function getHumanChoice(){
  //  value humanChoice.toLowerCase === computerChoice.toLowerCase;
 
-    if (question == "Rock"){
-        return "You lose! Rock beats Scissors";
-    } else if (question == "Paper"){
-        return "You lose! Paper beats Rock";
-    } else if (question == "Scisscor"){
-        return "You lose! Scisscor beats Paper";
+    if (question == "rock"){
+        //return "You lose! Rock beats Scissors";
+        return "rock"
+    } else if (question == "paper"){
+        //return "You lose! Paper beats Rock";
+        return "paper"
+    } else if (question == "scissors"){
+        //return "You lose! Scisscor beats Paper";
+        return "scissors"
     } else {
         return "Write Your Answer...";
     }
 }
 
+
 const humanScore = 0;
 const computerScore = 0;
-
+    
 function playRound(humanChoice, computerChoice){
-    if (humanChoice == "Rock" && )
+    if (humanChoice === "rock" && computerChoice === "scissors"){
+
+        return "Amazing, Rock beats Scissors!";
+        
+        } else if ( humanChoice === "rock" && computerChoice === "paper"){
+        
+        return "You lose, Paper beats Rock!";
+        
+        } else if (humanChoice === "rock" && computerChoice === "rock"){
+        
+        return "So close, it is a tie. Try again!";
+        
+        } else if (humanChoice === "paper" && computerChoice === "rock"){
+        
+        return "Amazing, Paper beats Rock!";
+        
+        } else if ( humanChoice === "paper" && computerChoice === "scissors"){
+        
+        return "You lose, Scissors beats paper!";
+        
+        } else if (humanChoice === "paper" && computerChoice === "paper"){
+        
+        return "So close, it is a tie. Try again!";
+        
+        } else if (humanChoice === "scissors" && computerChoice === "paper"){
+        
+        return "Amazing, Scissors beats Paper!";
+        
+        } else if ( humanChoice === "scissors" && computerChoice === "rock"){
+        
+        return "You lose, Rock beats Scissors!";
+        
+        } else if (humanChoice === "scissors" && computerChoice === "scissors"){
+        
+        return "So close, it is a tie. Try again!";
+        
+        } else {
+        
+        return "Write Your Answer";
+        
+        }
 }
+
+
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-
 console.log(playRound(humanSelection, computerSelection));
-console.log(getComputerChoice(2));
-console.log(question);
-console.log(playRound(Rock));
+
+
+
